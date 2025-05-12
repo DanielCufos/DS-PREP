3888be3a1b021cf999f9f02a20b541edb8028389
Daniel Cufos <danielcufos111@gmail.com>
2025-05-12 16:55:03 -0300

    Agregue 3 rutas con argumentos

--- .venv/hello.py
+++ .venv/hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -13,4 +14,25 @@ def hasta_luego():
 @app.route("/Buenas_tardes")
 def buenas_tardes():
     return "<p>Buenas Tardes!</p>"
+
+
+@app.route('/usuario/<username>')
+def mostrar_perfil_usuario(username):
+    return f'Usuario {escape(username)}'
+
+@app.route('/verificar-aprobacion/<int:nota>')
+def verificar_aprobacion(nota):
+    if nota >= 7:
+        return "Aprobado"
+    else: 
+        return "Desaprobado"
+
+@app.route('/path/<path:subpath>')
+def mostrar_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
+
+@app.route('/sumar/<int:numero>/<int:numero2>')
+def suma(numero, numero2):
+    resultado = numero + numero2
+    return f'Resultado = {(resultado)}'
 
