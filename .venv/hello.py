5645091b9a0a6608e430f5fefd405b8cdeb2c9a7
Daniel Cufos <danielcufos111@gmail.com>
2025-05-12 16:01:26 -0300

    Agregue 2 rutas simples

--- .venv/hello.py
+++ .venv/hello.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/Hasta_luego")
+def hasta_luego():
+    return "<p>Hasta Luego!</p>"
+
+@app.route("/Buenas_tardes")
+def buenas_tardes():
+    return "<p>Buenas Tardes!</p>"
+
