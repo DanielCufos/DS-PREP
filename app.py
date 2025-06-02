4d124f6e415af759c893a259951710f699d57593
Daniel Cufos <danielcufos111@gmail.com>
2025-06-02 16:09:06 -0300

    agregue rutas con url_for con las rutas que ya tenia hechas antes

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, url_for    
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/Hola")
 def hello_world():
     return "<p>Hello, World!</p>"
 
@@ -36,3 +36,16 @@ def suma(numero, numero2):
     resultado = numero + numero2
     return f'Resultado = {(resultado)}'
 
+@app.route('/')
+def links():
+    return f"""
+    <a href="{url_for('hello_world')}">Hola</a>
+    <div>
+    <a href="{url_for('hasta_luego')}">Adios</a>
+    <div>
+    <a href="{url_for('buenas_tardes')}">Buenas</a>
+    <div>
+    <a href="{url_for('mostrar_perfil_usuario', username= 'Adrian')}">usuario</a>
+    <div>
+    <a href="{url_for('verificar_aprobacion', nota= '8')}">Verificar Nota</a>
+"""
